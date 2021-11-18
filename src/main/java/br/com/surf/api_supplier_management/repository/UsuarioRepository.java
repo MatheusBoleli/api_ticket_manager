e899a465e05c18f961269d2e470c369c1664dfd5
MatheusBoleli <matheusboleli12@hotmail.com>
2021-11-18 17:47:28 -0300

    Comitando toda a lógica de autenticação.

--- src/main/java/br/com/surf/api_supplier_management/repository/UsuarioRepository.java
+++ src/main/java/br/com/surf/api_supplier_management/repository/UsuarioRepository.java
@@ -1,5 +1,7 @@
 package br.com.surf.api_supplier_management.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -8,6 +10,6 @@ import br.com.surf.api_supplier_management.model.Usuario;
 @Repository
 public interface UsuarioRepository extends JpaRepository<Usuario, Long>{
 	
-	Usuario findByNome(String usuario);
+	Optional<Usuario> findByEmail(String email);
 	
 }
