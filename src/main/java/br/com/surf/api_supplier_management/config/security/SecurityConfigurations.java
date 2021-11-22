e18bd7820d80a0049c12f1f8e9606e1f29031ae4
MatheusBoleli <matheusboleli12@hotmail.com>
2021-11-22 10:40:57 -0300

    Finalizando Autenticação.

--- src/main/java/br/com/surf/api_supplier_management/config/security/SecurityConfigurations.java
+++ src/main/java/br/com/surf/api_supplier_management/config/security/SecurityConfigurations.java
@@ -1,44 +1,62 @@
 package br.com.surf.api_supplier_management.config.security;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+
+import br.com.surf.api_supplier_management.repository.UsuarioRepository;
 
 @EnableWebSecurity
 @Configuration
 public class SecurityConfigurations extends WebSecurityConfigurerAdapter{
 	
+	@Autowired
+	private AutenticacaoService autenticacaoService;
+	
+	@Autowired
+	private TokenService tokenService;
+	
+	@Autowired
+	private UsuarioRepository usuarioRepository;
+	
+	@Bean
+	protected AuthenticationManager AuthenticationManager() throws Exception{
+		return super.authenticationManager();
+	}
+	
 	//Configurações de Autenticação
 	@Override
 	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-		super.configure(auth);
+		auth.userDetailsService(autenticacaoService).passwordEncoder(new BCryptPasswordEncoder());
 	}
 	
 	//Configurações de Autorização
+	
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 
 		http.authorizeRequests()
-		.antMatchers(HttpMethod.GET, "/ticket").permitAll()
-		.antMatchers(HttpMethod.GET, "/ticket/*").permitAll()
-		.antMatchers(HttpMethod.GET, "/catalogoAbertura").permitAll()
+		.antMatchers(HttpMethod.POST, "/auth").permitAll()
+		.anyRequest().authenticated().and().csrf().disable()
+		.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+		.and().addFilterBefore(new AutenticacaoViaTokenFilter(tokenService, usuarioRepository), UsernamePasswordAuthenticationFilter.class);
 
 		;
-		
-		
-		super.configure(http);
 	}
 	
 	//Configurações de recursos estaticos (css, js, hmtl, imagens, etc)
 	@Override
 	public void configure(WebSecurity web) throws Exception {
-		super.configure(web);
-	}
-	
-	
-	
+	}	
 }
