e36dffdaa227c9bcb63c2910517c68bbe72c1463
MatheusBoleli <matheusboleli12@hotmail.com>
2021-11-30 09:32:50 -0300

    commit

--- src/main/java/br/com/surf/api_supplier_management/controller/AutenticacaoController.java
+++ src/main/java/br/com/surf/api_supplier_management/controller/AutenticacaoController.java
@@ -3,7 +3,6 @@ package br.com.surf.api_supplier_management.controller;
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Profile;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
@@ -20,7 +19,6 @@ import br.com.surf.api_supplier_management.form.LoginForm;
 
 @RestController
 @RequestMapping("/auth")
-@Profile("prod")
 public class AutenticacaoController {
 	
 	@Autowired
