6f8f4c1ab4e97e674d4c37b0c3fb10f847c45087
MatheusBoleli <matheusboleli12@hotmail.com>
2021-12-01 11:01:25 -0300

    Realizando Inserção de Profiles de Produção e Desenvolvimento.

--- src/main/java/br/com/surf/api_supplier_management/config/security/SecurityConfigurations.java
+++ src/main/java/br/com/surf/api_supplier_management/config/security/SecurityConfigurations.java
@@ -3,6 +3,7 @@ package br.com.surf.api_supplier_management.config.security;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
@@ -17,6 +18,7 @@ import br.com.surf.api_supplier_management.repository.UsuarioRepository;
 
 @EnableWebSecurity
 @Configuration
+@Profile("prod")
 public class SecurityConfigurations extends WebSecurityConfigurerAdapter{
 	
 	@Autowired
